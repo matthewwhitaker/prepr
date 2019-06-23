615c0ff3b0e180b63922fe9173dfe824ba6c5fdd
matt <mwhitaker1006@gmail.com>
2019-06-23 12:06:19 +0100

    added feature testing for template rendering

--- spec/features/visiting_homepage_spec.rb
+++ spec/features/visiting_homepage_spec.rb
@@ -3,4 +3,23 @@ feature 'Visiting homepage' do
     visit('/')
     expect(page).to have_current_path('/posts')
   end
+
+  scenario 'navbar renders correctly when signed in' do
+    create_test_user()
+    sign_in_test_user()
+    page.find_link(class: 'navbar-brand')
+    page.find_button('Add new post')
+    page.find_button('Sign out')
+    expect(page).not_to have_button('Sign in')
+    expect(page).not_to have_button('Sign up')
+  end
+
+  scenario 'navbar renders correctly when signed out' do
+    visit('/')
+    page.find_link(class: 'navbar-brand')
+    page.find_button('Sign in')
+    page.find_button('Sign up')
+    expect(page).not_to have_button('Add new post')
+    expect(page).not_to have_button('Sign out')
+  end
 end
